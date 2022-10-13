66fea4bf409b60c0ce20d7907868a06cffaf945d
OnkelPony <zkurvenej@outlook.com>
2022-10-13 11:32:06 +0200

    Initial commit

---
+++ CPiscineC00/ex01/ft_print_alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void    ft_print_alphabet(void)
+{
+    char ch;
+    ch = 'a';
+    while (ch <= 'z')
+    {
+        write(1, &ch, 1);
+        ch++;
+    }
+}
+
+int main(void)
+{
+    ft_print_alphabet();
+}
