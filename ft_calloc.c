186ad26bab8be92fc5129ad5ce9cdb277842602e
Jiri Martinek <jimartin@c2r4s3.42prague.com>
2023-02-07 12:33:22 +0100

    Libft final fix.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jimartin <jimartin@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 13:09:50 by jimartin          #+#    #+#             */
-/*   Updated: 2023/02/03 14:15:47 by jimartin         ###   ########.fr       */
+/*   Updated: 2023/02/07 12:31:39 by jimartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,14 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*result;
 
+	if (nmemb == 0 || size == 0)
+	{
+		result = malloc(0);
+		return (result);
+	}
 	if (nmemb >= INT_MAX || size >= INT_MAX)
 	{
-		return (0);
+		return (NULL);
 	}
 	result = malloc(nmemb * size);
 	if (result == NULL)
